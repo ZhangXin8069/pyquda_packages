35035682408dd47d41e28344d77c82f200587c07
zhangxin <zhangxin8069@qq.com>
2023-07-25 18:53:57 +0800

    2023-07-25-18-53-57

--- env.sh
+++ env.sh
@@ -1,10 +1,9 @@
-file_path="./"
-file_path=$(cd ${file_path} && pwd)
-install_path="/home/aistudio/external-libraries" #absolute path and same to quda install_path
+file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
 mkdir ${install_path} -p
 
 pushd ~
-echo "export PYTHONPATH=${install_path}:\$PYTHONPATH" >> ~/.bashrc 
-echo "export LD_LIBRARY_PATH=${install_path}/quda-develop/build/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc 
+echo "export PYTHONPATH=${install_path}:\$PYTHONPATH" >>~/.bashrc
+echo "export LD_LIBRARY_PATH=${install_path}/quda-develop/build/lib:\$LD_LIBRARY_PATH" >>~/.bashrc
 source ./.bashrc
 popd
