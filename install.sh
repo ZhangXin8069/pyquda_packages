a245d3b363c8295e7a523b8dd7cb96d1381ea272
zhangxin <zhangxin8069@qq.com>
2023-07-25 22:42:38 +0800

    2023-07-25-22-42-38

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 source ./env.sh
 
 pushd ${install_path}
-pip install -U pip numpy mpi4py cupy-cuda11x Cython typing_extensions -t ${install_path} #cupy-cuda11x:rely on `nvcc --version`,more in https://docs.cupy.dev/en/stable/install.html
+pip install -U numpy mpi4py cupy-cuda11x Cython typing_extensions -t ${install_path} #cupy-cuda11x:rely on `nvcc --version`,more in https://docs.cupy.dev/en/stable/install.html
 tar xzf ${file_path}/PyQuda-master.tar.gz && pushd PyQuda-master
 cp ../quda-develop/build/lib/libquda.so ./
 pip install . -t ${install_path}
 python tests/test.clover.py
 popd
 popd
+# note
+## change external-libraries/pyquda/utils/source.py and external-libraries/pyquda/field.py
+## as
+## from typing import List, Union
+## from typing_extensions import Literal
+## to debug
