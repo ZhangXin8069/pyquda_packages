7495a0621c923b41812cad39e9db08436ba5ab6d
zhangxin <zhangxin8069@qq.com>
2023-07-25 20:30:59 +0800

    2023-07-25-20-30-59

--- env.sh
+++ env.sh
@@ -3,8 +3,5 @@ file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 install_path=$(cd ~/external-libraries && pwd) #absolute path
 mkdir ${install_path} -p
 
-pushd ~
-echo "export PYTHONPATH=${install_path}:\$PYTHONPATH" >>~/.bashrc
-echo "export LD_LIBRARY_PATH=${install_path}/quda-develop/build/lib:\$LD_LIBRARY_PATH" >>~/.bashrc
-source ./.bashrc
-popd
+export PYTHONPATH=${install_path}:\$PYTHONPATH
+export LD_LIBRARY_PATH=${install_path}/quda-develop/build/lib:\$LD_LIBRARY_PATH
