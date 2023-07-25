1b12620c8ce8ba24e8409cf3653bff7a5f698899
zhangxin <zhangxin8069@qq.com>
2023-07-25 09:59:55 +0800

    2023-07-25-09-59-55

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ source ./env.sh
 
 pushd ${install_path}
 pip install -U pip numpy mpi4py cupy-cuda11x Cython typing_extensions -t ${install_path} #cupy-cuda11x:rely on `nvcc --version`,more in https://docs.cupy.dev/en/stable/install.html
-unzip -oq ${file_path}/PyQuda-master.zip && pushd PyQuda-master
+tar xzf ${file_path}/PyQuda-master.tar.gz && pushd PyQuda-master
 cp ../quda-develop/build/lib/libquda.so ./
 pip install . -t ${install_path}
 python tests/test.clover.py
