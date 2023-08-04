1a5b94e87d672d64bd16507037d730e4bd8368be
zhangxin <zhangxin8069@qq.com>
2023-08-04 17:28:18 +0800

    2023-08-04-17-28-18

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-install_path="$HOME/external-libraries" #absolute path
+# install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
+install_path=${HOME}/external-libraries #absolute path
 mkdir ${install_path} -p
 
 export PYTHONPATH=${install_path}:\$PYTHONPATH
